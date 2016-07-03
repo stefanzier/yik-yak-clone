71c21368c0c3e7137b5c570a01e44fc3304c1b5e
Stefan Zier <szier@icloud.com>
2016-07-03 11:32:21 -0700

    Refactored Post Component

--- client/components/post.js
+++ client/components/post.js
@@ -1,20 +1,67 @@
 import React, { Component } from 'react';
 import { Card, CardText, CardHeader } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
-const Post = ({post}) => {
-  const { content, createdAt } = post;
-  return (
-    <div className="post-container">
-      <Card>
-        <CardText>
-          {content}
-          <p className="timestamp">
-            {moment(createdAt).fromNow()}
-          </p>
-        </CardText>
-      </Card>
-    </div>
-  );
+class Post extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      disabledUpvote: false,
+      disabledDownvote: false
+    }
+
+    this.handleUpvoteClicked = this.handleUpvoteClicked.bind(this);
+    this.handleDownvoteClicked = this.handleDownvoteClicked.bind(this);
+  }
+
+  handleUpvoteClicked() {
+    if (!this.state.disabledUpvote) {
+      this.setState({
+        disabledUpvote: true,
+        disabledDownvote: false
+      });
+    }
+  }
+  
+  handleDownvoteClicked() {
+    if (!this.state.disabledDownvote) {
+      this.setState({
+        disabledUpvote: false,
+        disabledDownvote: true
+      });
+    }
+  }
+
+  render() {
+    const { content, createdAt, votes } = this.props.post;
+
+    return (
+      <div className="post-container">
+        <Card>
+          <CardText>
+            <p className="timestamp">
+              {moment(createdAt).fromNow()}
+            </p>
+            {content}
+            <div style={{ textAlign: "right" }}>
+              <FlatButton 
+                label="Upvote" 
+                primary={true} 
+                disabled={this.state.disabledUpvote}
+                onClick={this.handleUpvoteClicked} />
+              <FlatButton 
+                label="Downvote" 
+                secondary={true}
+                disabled={this.state.disabledDownvote}
+                onClick={this.handleDownvoteClicked} />
+            </div>
+          </CardText>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default Post;
